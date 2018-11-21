095837f0965ba5bdf3a7b4baa40985e41799fe81
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-11-21 11:35:16 +0200

    run

--- run-docker-cli.sh
+++ run-docker-cli.sh
@@ -2,10 +2,6 @@
 
 CURRENT_DIR=$(dirname $(readlink -f $0))
 
-docker-compose -f ./docker/compose.yml ps -q php56 2> /dev/null > /dev/null
-
-if [[ $? -eq 1 ]]; then
-    docker-compose -f $CURRENT_DIR/docker/compose.yml up -d
-fi
+docker-compose -f $CURRENT_DIR/docker/compose.yml up -d
 
 docker-compose -f $CURRENT_DIR/docker/compose.yml exec php56 bash
--- run-docker-tests.sh
+++ run-docker-tests.sh
@@ -2,15 +2,11 @@
 
 CURRENT_DIR=$(dirname $(readlink -f $0))
 
-docker-compose -f ./docker/compose.yml ps -q php56 2> /dev/null > /dev/null
-
-if [[ $? -eq 1 ]]; then
-    docker-compose -f $CURRENT_DIR/docker/compose.yml up -d
-fi
+docker-compose -f $CURRENT_DIR/docker/compose.yml up -d
 
 mongoVersions=("24" "26" "30" "32" "33" ""34)
 
 for mongoVersion in ${mongoVersions[@]}; do
     echo -e "\n\n\033[1;33m##### Executing test for Mongo v.$mongoVersion\033[0m"
-    docker-compose -f ./docker/compose.yml exec php56 bash -c "export PHPMONGO_DSN=mongodb://mongodb$mongoVersion; /phpmongo/vendor/bin/phpunit -c /phpmongo/tests/phpunit.xml /phpmongo/tests"
+    docker-compose -f $CURRENT_DIR/docker/compose.yml exec php56 bash -c "export PHPMONGO_DSN=mongodb://mongodb$mongoVersion; /phpmongo/vendor/bin/phpunit -c /phpmongo/tests/phpunit.xml /phpmongo/tests"
 done
